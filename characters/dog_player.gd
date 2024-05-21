292121de93da8d893943fcd88f9cf48a32ea58d6
beatrizps <beatrizpatriciost@gmail.com>
2024-05-21 16:53:15 -0300

    documentação

--- characters/dog_player.gd
+++ characters/dog_player.gd
@@ -15,20 +15,23 @@ var moving = false
 var animation_speed = 2
 @onready var ray = $RayCast2D
 
+# Inicializa a direção do player e ajusta posição centralizado na tile
 func _ready():
 	update_animation_parameters(starting_direction)
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
+	
 func _process(delta):
 	print(ray.is_colliding())
-	
+
+# Recebe os eventos de teclado
 func _unhandled_input(event):
 	if moving:
 		return
 	for dir in inputs.keys():
 		if event.is_action(dir):
 			move(dir)
-
+# Função de movimentação do player
 func move(dir):
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
@@ -41,12 +44,14 @@ func move(dir):
 		await tween.finished
 		moving = false
 	pick_new_state()
-	
+
+# Atualização de animação
 func update_animation_parameters(move_input: Vector2):
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/idle/blend_position", move_input)
 		animation_tree.set("parameters/walk/blend_position", move_input)
-		
+
+# Transição entre animação parado e movimento
 func pick_new_state():
 	if(moving):
 		state_machine.travel("walk")
